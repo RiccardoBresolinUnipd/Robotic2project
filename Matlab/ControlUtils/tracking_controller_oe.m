1a485c778347e23ba343007065f94f89b679073b
ltrubian <trubian.lorenzo@gmail.com>
2025-12-15 09:44:08 +0100

    First iteration of output-error feedback control scheme

---
+++ Matlab/ControlUtils/tracking_controller_oe.m
@@ -0,0 +1,22 @@
+function [v, w] = tracking_controller_oe(xd, yd, thetad, x, y, theta, vd, wd, linear)
+
+eps_v = 1e-6;
+b = 0.75;
+
+T = [cos(theta), -b*sin(theta);
+    sin(theta),   b*cos(theta)];
+Tinv = [cos(theta), sin(theta);
+    -sin(theta)/b,  cos(theta)/b];
+
+y_real = [x; y] + b*[cos(theta); sin(theta)];
+y_des = [xd; yd] + b*[cos(thetad); sin(thetad)];
+yd_des = T * [vd; wd];
+
+K = [1; 1] * 50;
+
+u = K .* (y_des - y_real) + yd_des;
+
+% ---- input transformation ----
+v = Tinv(1,:) * u;
+w = Tinv(2,:) * u;
+end
