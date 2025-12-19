fa947d24a0407fe3c62083839c407f368bca04c3
ltrubian <trubian.lorenzo@gmail.com>
2025-12-19 12:08:47 +0100

    laboratory session: tracking controller with B point

--- Matlab/ControlTest.m
+++ Matlab/ControlTest.m
@@ -12,12 +12,14 @@ y_curr = 0;
 theta_curr = 0;
 
 % controller
-linear = false;      % true = linear controller  - false = non-linear controller
+linear = true;      % true = linear controller  - false = non-linear controller
 
 % Vectors to save the simulation
 x = zeros(length(t),1);
 y = zeros(length(t),1);
 theta = zeros(length(t),1);
+v_r = zeros(length(t),1);
+w_r = zeros(length(t),1);
 
 %% ---------------- Trajectory ----------------------
 list = {"custom","square","triangle", "oval", "circle"};
@@ -44,22 +46,43 @@ for k = 1:length(t)
 
     % Differential flatness
     [x_d, y_d, theta_d, vd, wd] = differential_flatness(x_d, y_d, dx_d, dy_d, ddx_d, ddy_d);
+    % theta_d = wrapToPi(theta_d);
 
     % Tracking controller
-    [v, w] = tracking_controller(x_d, y_d, theta_d, x_curr, y_curr, theta_curr, vd, wd, linear);
+    [v, w] = tracking_controller_oe(x_d, y_d, theta_d, x_curr, y_curr, theta_curr, vd, wd, linear);
+    v_r(k) = v;
+    w_r(k) = w;
 
     % unicycle
     [x_curr, y_curr, theta_curr] = unicycle_model(x_curr, y_curr, theta_curr, v, w, dt);
+    % theta_curr = wrapToPi(theta_curr);
 
 end
-
-save('data.mat', 'x', 'y', 'xd', 'yd');
-
 %% -------------------- PLOT RESULTS ---------------------------
-figure; hold on; grid on; axis equal;
-plot(xd, yd, 'r--', 'LineWidth',1.5);
-plot(x, y, 'b', 'LineWidth',2);
+fig = figure('Name', 'Robot Tracking', 'Units', 'normalized', 'OuterPosition', [0, 0, 1, 1]);
+
+% Subplot 1: Maze (Left Left)
+axesMaze = subplot(2, 2, 1);
+hold(axesMaze, 'on');
+grid(axesMaze, 'on');
+axis(axesMaze, "square");
+plot(axesMaze, xd, yd, 'r--', 'LineWidth',1.5);
+plot(axesMaze, x, y, 'b', 'LineWidth',2);
 legend("Desired Trajectory", "Robot");
 title("Tracking");
 xlabel("x"); ylabel("y");
 
+axesMaze = subplot(2, 2, 3);
+hold(axesMaze, 'on');
+grid(axesMaze, 'on');
+plot(axesMaze, t, w_r); legend("w")
+
+axesMaze = subplot(2, 2, 2);
+hold(axesMaze, 'on');
+grid(axesMaze, 'on');
+plot(axesMaze, t, x - xd', t, y-yd'); legend("x", "y") %, "xdes", "ydes")
+
+axesMaze = subplot(2, 2, 4);
+hold(axesMaze, 'on');
+grid(axesMaze, 'on');
+plot(axesMaze, t, theta);
