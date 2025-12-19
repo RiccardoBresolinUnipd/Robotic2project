2c9c3270cb3198805f114d0b1c7355eda8c3bd5f
ltrubian <trubian.lorenzo@gmail.com>
2025-12-19 21:10:01 +0100

    We may have a controller with B point. I don't get how
    Idk how it works, still working

--- Matlab/ControlUtils/tracking_controller_oe.m
+++ Matlab/ControlUtils/tracking_controller_oe.m
@@ -1,7 +1,7 @@
 function [v, w, y_real, y_des] = tracking_controller_oe(xd, yd, thetad, x, y, theta, vd, wd, linear)
 
 eps_v = 1e-6;
-b = 0.75;
+b = 0.2;
 
 T = [cos(theta), -b*sin(theta);
     sin(theta),   b*cos(theta)];
@@ -10,12 +10,12 @@ Tinv = [cos(theta), sin(theta);
     -sin(theta)/b,  cos(theta)/b];
 
 y_real = [x; y] + b*[cos(theta); sin(theta)];
-y_des = [xd; yd] + b*[cos(thetad); sin(thetad)];
+y_des = [xd; yd] + 0*[cos(thetad); sin(thetad)];
 yd_des = T * [vd; wd];
 
-K = [1; 1] * 50;
+K = [1; 1] * 2;
 
-u = K .* (y_des - y_real) + yd_des;
+u = K .* (y_des - y_real); % + yd_des;
 
 % ---- input transformation ----
 v = Tinv(1,:) * u;
