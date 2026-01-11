6fda105fe86d2f08cd4eb2d6aaaf7bb08f0109b5
ltrubian <trubian.lorenzo@gmail.com>
2026-01-11 15:29:57 +0100

    trajectory comparison plot

--- Matlab/analysis.m
+++ Matlab/analysis.m
@@ -22,8 +22,8 @@ cm.ddXd(:,1:2) = [ddxd', ddyd'];
 
 clearvars -except bp cm t shapes select
 
-%% plots
-
+%% plots errors
+save = false;
 xlimts = [5.5 7.5];
 
 figure(Name="Errors X")
@@ -33,8 +33,10 @@ if strcmp(shapes{select}, "square")
     xlim(xlimts); ylim([-0.25, 0.05]);
 end
 legend("CoM", "Bp"); xlabel("t [s]"); ylabel("Tracking error [m]"); title("Error X")
-f = gcf;
-exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_ErrorX.png", "Resolution",300)
+if save
+    f = gcf;
+    exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_ErrorX.png", "Resolution",300)
+end
 
 
 figure(Name="Errors Y")
@@ -44,9 +46,13 @@ if strcmp(shapes{select}, "square")
     xlim(xlimts); ylim([-0.25, 0.05]);
 end
 legend("CoM", "Bp"); xlabel("t [s]"); ylabel("Tracking error [m]"); title("Error Y")
-f = gcf;
-exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_ErrorY.png", "Resolution",300)
+if save
+    f = gcf;
+    exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_ErrorY.png", "Resolution",300)
+end
 
+%% plots Inputs
+save = false;
 
 figure(Name="Input v")
 plot(t, cm.U(:,1), t, bp.U(:,1), LineWidth=1.5 )
@@ -57,9 +63,10 @@ else
     ylim([0 1.5])
 end
 legend("CoM", "Bp"); xlabel("t [s]"); ylabel("v [m/s]"); title("Input v")
-f = gcf;
-exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_InputV.png", "Resolution",300)
-
+if save
+    f = gcf;
+    exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_InputV.png", "Resolution",300)
+end
 
 figure(Name="Input w")
 plot(t, cm.U(:,2), t, bp.U(:,2), LineWidth=1.5 )
@@ -70,6 +77,42 @@ else
     ylim([-2 2])
 end
 legend("CoM", "Bp"); xlabel("t [s]"); ylabel("w [rad/s]"); title("Input w")
-f = gcf;
-exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_InputW.png", "Resolution",300)
+if save
+    f = gcf;
+    exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_InputW.png", "Resolution",300)
+end
 
+%% plots trajectories
+save = true;
+
+figure(Name="Trajectory"); 
+total = subplot(2,2,[1 3]);
+grid(total, "on"); hold(total,"on");
+plot(total, cm.X(:,1), cm.X(:,2), LineWidth=1.5 );
+plot(total, bp.X(:,1), bp.X(:,2), LineWidth=1.5 ); 
+plot(total, cm.Xd(:,1), cm.Xd(:,2), "--", LineWidth=3); 
+legend(total,"CoM", "Bp", "Desired"); xlabel("x [m]"); ylabel("y [m]");
+
+zoom1 = subplot(2,2,2); 
+grid(zoom1, "on"); hold(zoom1,"on");
+xlim(zoom1, [2.2 4.2]); ylim(zoom1, [2 2.8]);
+plot(zoom1, cm.X(:,1), cm.X(:,2), LineWidth=1.5 ); 
+plot(zoom1, bp.X(:,1), bp.X(:,2), LineWidth=1.5 );
+plot(zoom1, cm.Xd(:,1), cm.Xd(:,2), "--", LineWidth=3); 
+legend(zoom1, "CoM", "Bp", "Desired");
+xlabel("x [m]"); ylabel("y [m]");
+
+zoom2 = subplot(2,2,4); 
+grid(zoom2, "on"); hold(zoom2,"on");
+xlim(zoom2, [4.2 5.2]); ylim(zoom2, [-3.8 -3]);
+plot(zoom2, cm.X(:,1), cm.X(:,2), LineWidth=1.5 ); 
+plot(zoom2, bp.X(:,1), bp.X(:,2), LineWidth=1.5 );
+plot(zoom2, cm.Xd(:,1), cm.Xd(:,2), "--", LineWidth=3); 
+legend(zoom2, "CoM", "Bp", "Desired");
+xlabel("x [m]"); ylabel("y [m]");
+
+
+if save
+    f = gcf;
+    exportgraphics(gcf, ".\corrected_images\" + shapes{select} + "_trajectory.png", "Resolution",300)
+end
